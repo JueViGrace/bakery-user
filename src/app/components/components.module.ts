d11a23a44f25994336a23ca563d0141eff794331
Daniel Zabala <danielzabalanava@gmail.com>
2024-01-05 08:23:04 -0400

    new repo

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './loader/loader.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [LoaderComponent, FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [FooterComponent, HeaderComponent],
+})
+export class ComponentsModule {}
